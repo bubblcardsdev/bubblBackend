411569a3c6a8f6ad734097224d84ee75c1d159df
mynervadev <developer@mynerva.in>
2024-09-26 15:57:10 +0530

    test

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
 import app from "./app.js";
 import router from "./routes/index.route.js";
 import adminRouter from "./routes/adminRoutes/index.admin.route.js";
-import bodyParser from "body-parser";
 
 app.use("/api", router);
 app.use("/api/admin", adminRouter);
-app.use(bodyParser.json({ limit: "100mb" }));
-app.use(
-  bodyParser.urlencoded({
-    limit: "100mb",
-    extended: true,
-  })
-);
 
 const PORT = process.env.PORT;
 
