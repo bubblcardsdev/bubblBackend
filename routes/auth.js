150b684a64e52eef3aa047fc75f1decfbe895437
tharunofficial12 <tharun@bubbl.cards>
2025-07-28 14:35:10 +0530

    c

--- routes/auth.js
+++ routes/auth.js
@@ -1,3 +1,4 @@
+/* eslint-disable linebreak-style */
 import express from "express";
 import {
   login,
@@ -16,20 +17,25 @@ import {
   resetPassword,
   verifyEmailOtp,
   resendMailOtp,
-  verifyAppleUser,
-  createUserBulkController,
+  createUserMobile, 
+
 } from "../controllers/auth.js";
 import { authenticateToken } from "../middleware/token.js";
+import {SSE, verifyMail, sendEmail, emailVerified, sendForgetPassword} from "../helper/socket_server.js";
 
 const router = express.Router();
 
+router.get("/events",SSE);
+router.post("/sendEmail",sendEmail);
+router.post("/sendForgetPasswordEmail",sendForgetPassword);
+router.get("/verify",verifyMail);
+router.get("/email-verified",emailVerified);
 router.post("/token", issueNewToken);
 router.post("/register", createUser);
-router.post("/registerBulk", createUserBulkController);
+router.post("/registerMobile", createUserMobile);
 router.post("/verifygoogleuser", verifyGoogleUser);
 router.post("/verifyfacebookuser", verifyFacebookUser);
 router.post("/verifylinkedinuser", verifyLinkedinUser);
-router.post("/verifyappleuser", verifyAppleUser);
 router.post("/addphonenumber", addPhoneNumber);
 router.post("/resendotp", resendOtp);
 router.post("/resendMailotp", resendMailOtp);
@@ -41,5 +47,7 @@ router.post("/forgotpassword", forgotPassword);
 router.post("/changepassword", changePassword);
 router.put("/updateuser", authenticateToken, updateUser);
 router.put("/reset", authenticateToken, resetPassword);
+
+
 
 export default router;
